87b39a70393d5999b73f0dd1d738a2bb5c324b46
usmail2paul <usmail2paul@gmail.com>
2024-10-22 04:13:26 -0400

    app.java revised

--- src/main/java/org/selenium/jenkins/App.java
+++ src/main/java/org/selenium/jenkins/App.java
@@ -7,42 +7,45 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebDriverException;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.io.FileHandler;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");        
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         WebDriver driver = new ChromeDriver(options);
-        driver.get("https://www.facebook.com/login.php");
+        driver.get("http://18.208.249.169:8081/contact.html");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
-        WebElement userId = driver.findElement(By.id("email"));
-        WebElement password = driver.findElement(By.id("pass"));
-  //     WebElement submitButton = driver.findElement(By.id("loginbutton"));
-        userId.sendKeys("test@selenium.assignment.com");
-        password.sendKeys("**Password**");
+        driver.findElement(By.id("inputName")).sendKeys("Paul Mosai");;
+        driver.findElement(By.id("inputNumber")).sendKeys("1210222024");;
+        driver.findElement(By.id("inputMail")).sendKeys("Paul.Mosai@pysalms.com");;
+        driver.findElement(By.id("inputMessage")).sendKeys("Capstone Project DevOps");;
+        driver.findElement(By.id("my-button")).click();
     	//capture screenshot
-        System.out.println("Selenium Jenkins Assignment!");
+        System.out.println("Selenium Capstone Integration!");
         TakesScreenshot capture =  ((TakesScreenshot)driver);
         File srcFile = capture.getScreenshotAs(OutputType.FILE);
-        File desFile = new File("Selenium-test-report.jpg");
+        File desFile = new File("capstone-test-report.jpg");
         try {
 			FileHandler.copy(srcFile, desFile);
-			System.out.println("screenshot captured. ");
+			System.out.println("screenshot captured and scripts executed. ");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
         driver.quit();
         
-    }
+    	}
     
 }
+
+
+    
+
